548bc553e4f7c8cb8012b6c6a1a2e20abb9540ba
Owais Malik <owaismalik@outlook.com>
2019-01-21 01:28:57 +0530

    pushing to github from local

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -3,10 +3,10 @@
     <div class="container">
       <a href="https://owais-articlesapi.herokuapp.com/" class="navbar-brand">Articles App</a>
       <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="https://github.com/thedemon786" target="_blank">Github</a>
-    </li>
-    </ul>
+        <li class="nav-item">
+          <a class="nav-link" href="https://github.com/thedemon786" target="_blank">Github</a>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
